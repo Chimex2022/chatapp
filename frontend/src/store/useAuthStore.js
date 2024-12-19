a17f61a1625a46a010f06bafdea5d00d346b1774
Anyaele <kirchoff200@gmail.com>
2024-12-19 17:05:47 +0100

    added

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,8 +3,8 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-// const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
-const BASE_URL = "https://chatapp-1-t4fn.onrender.com";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "https://chatapp-1-t4fn.onrender.com";
+  // const BASE_URL = "https://chatapp-1-t4fn.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
@@ -18,7 +18,6 @@ export const useAuthStore = create((set, get) => ({
   checkAuth: async () => {
     try {
       const res = await axiosInstance.get("/auth/check");
-
       set({ authUser: res.data });
       get().connectSocket();
     } catch (error) {
