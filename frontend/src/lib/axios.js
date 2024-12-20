0a719ddfde517177cc9abbe050de4e1d0d0aef74
Anyaele <kirchoff200@gmail.com>
2024-12-20 17:09:55 +0100

    added url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  BASE_URL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chatapp-1-t4fn.onrender.com",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chatapp-1-t4fn.onrender.com",
   withCredentials: true,
 });
