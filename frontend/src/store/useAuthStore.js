106ec7ba6933c0e653cce9f7d7b4501d13089d3e
Anyaele <kirchoff200@gmail.com>
2024-12-20 19:15:38 +0100

    added new

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-  // const BASE_URL = "https://chatapp-1-t4fn.onrender.com/api";
+  const BASE_URL = "localhost:5001/api";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
