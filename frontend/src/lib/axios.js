71d85641a379684dcc34ad96aaa791ae6c54b1f1
Anyaele <kirchoff200@gmail.com>
2024-12-20 19:06:02 +0100

    added adjust

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chatapp-1-t4fn.onrender.com/api",
+  BASE_URL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chatapp-1-t4fn.onrender.com/api",
   withCredentials: true,
 });
