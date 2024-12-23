c511dbdfcee826df74e773bec629b4b04dfa5c15
Anyaele <kirchoff200@gmail.com>
2024-12-23 17:20:51 +0100

    added

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,12 +15,10 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
+const allowedOrigins = ["https://chatapp-smoky-psi.vercel.app"];
 
 app.use(express.json());
 app.use(cookieParser());
-// app.use(cors({ origin: 'https://chatapp-smoky-psi.vercel.app/', credentials: true }));
-
-const allowedOrigins = ["https://chatapp-smoky-psi.vercel.app"];
 
 
 app.use("/api/auth", authRoutes);
@@ -32,13 +30,13 @@ app.use("/api/messages", messageRoutes);
     })
 );
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
