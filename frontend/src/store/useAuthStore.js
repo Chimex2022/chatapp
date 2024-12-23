0b284040fe044fecf86ddca5595794f6c01b2fbe
Anyaele <kirchoff200@gmail.com>
2024-12-23 17:29:02 +0100

    ade

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,8 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-  const BASE_URL = "https://chatapp-1-t4fn.onrender.com";
-
+const BASE_URL = "https://chatapp-1-t4fn.onrender.com";
 export const useAuthStore = create((set, get) => ({
   authUser: null,
   isSigningUp: false,
@@ -44,7 +43,7 @@ export const useAuthStore = create((set, get) => ({
   login: async (data) => {
     set({ isLoggingIn: true });
     try {
-      const res = await axiosInstance.post("/auth/login", data,   {
+      const res = await axiosInstance.post("/auth/login", data, {
         withCredentials: true, // Required for cookies/auth headers
       });
       set({ authUser: res.data });
