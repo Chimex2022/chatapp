ef1a40d9c3f32c75a94159561455c5bf71361b0f
Anyaele <kirchoff200@gmail.com>
2024-12-25 14:34:09 +0100

    mine

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://chatapp-backend-taupe.vercel.app/",
-  withCredentials: true,
+  baseURL: "https://chatapp-backend-taupe.vercel.app/"
 });
