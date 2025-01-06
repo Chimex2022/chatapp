ab5ae5fec94477722e07d578bbe1e6374f5f1cad
Anyaele <kirchoff200@gmail.com>
2025-01-06 01:50:08 +0100

    back

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,7 @@ app.get("/test", (req, res) => {
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'https://chatapp-smoky-psi.vercel.app' // Allow only this origin
+  origin: 'https://chatapp-smoky-psi.vercel.app || http://localhost:5174' // Allow only this origin
 }));
 
 
@@ -30,13 +30,13 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
