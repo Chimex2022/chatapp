ea5ea8c319fe5ce34201bb2b59ebf73dde8d48db
Anyaele <kirchoff200@gmail.com>
2025-01-06 01:17:53 +0100

    comment

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,16 +27,16 @@ app.use(cors({
 
 // app.use(cors({origin: "*", credentials: true}));
 
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
+// app.use("/api/auth", authRoutes);
+// app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
 
 server.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
