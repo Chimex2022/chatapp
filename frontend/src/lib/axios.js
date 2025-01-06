876b51fff781a95ddd4e7c88a792d7270103af42
Anyaele <kirchoff200@gmail.com>
2025-01-06 01:48:56 +0100

    added

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_DOMAIN = import.meta.env.MODE === "development" ? "http://localhost:5001" : "https://chatapp-production-d8dd.up.railway.app";
+
 export const axiosInstance = axios.create({
-  baseURL: "https://chatapp-production-d8dd.up.railway.app/",
+  baseURL: API_DOMAIN,
   headers: {
     "Content-Type": "application/json",
   }
