cdecae1ff6302f7d8addeb97a110351301e62d5b
Anyaele <kirchoff200@gmail.com>
2025-01-06 14:26:16 +0100

    added

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
 
-const BASE_URL = "https://chatapp-production-d8dd.up.railway.app/api";
+const BASE_URL = "https://chatapp-production-d8dd.up.railway.app";
 
 
 export const useAuthStore = create((set, get) => ({
